c2efae76646889d6b635f35bd0837b26072c2e49
guanjie sun <gjsun255@stu.just.edu.cn>
2025-07-13 18:39:41 +0800

    add concurrent feature to http server

--- http_server.py
+++ http_server.py
@@ -1,18 +1,21 @@
 import socket
+import threading
 
 
-def handle_client(client_socket):
+def handle_client(conn_sock):
     """处理客户端请求的函数"""
+    client_addr = conn_sock.getpeername()
+    print(f"[{threading.current_thread().name}] handling [client {client_addr}]")
+
     try:
         while True:
-            request = client_socket.recv(1024).decode()
+            request = conn_sock.recv(1024).decode()
             if not request:
                 # 客户端端开连之后, 会发送一个空请求, 这时可以认为客户端已经断开连接
-                print(f"[Client {client_socket.getpeername()}] disconnected.")
+                print(f"[Client {client_addr}] disconnected.")
                 break
 
-            request_line = request.splitlines()[0]  # 获取请求行
-            print(f"Request line: {request_line}")
+            request_line = request.splitlines()[0]             # 获取请求行
             method, path, http_version = request_line.split()  # 解析请求行, 获取请求方法, 路径和 HTTP 版本
 
             if method == 'GET':
@@ -25,6 +28,7 @@ def handle_client(client_socket):
                     f"{http_version} 200 OK\r\n"
                     "Content-Type: text/html; charset=utf-8\r\n"
                     f"Content-Length: {len(body.encode('utf-8'))}\r\n"
+                    "Connection: keep-alive\r\n"
                     "\r\n"
                     f"{body}"
                 )
@@ -34,17 +38,18 @@ def handle_client(client_socket):
                     f"{http_version} 405 Method Not Allowed\r\n"
                     "Content-Type: text/html\r\n"
                     f"Content-Length: {len(body.encode('utf-8'))}\r\n"
+                    "Connection: keep-alive\r\n"
                     "\r\n"
                     f"{body}"
                 )
-            client_socket.sendall(response.encode())
+            conn_sock.sendall(response.encode())
     finally:
-        client_socket.close()
-        print("Client socket closed.\n")
+        conn_sock.close()
+        print("Conn socket closed.\n")
 
 
 def main():
-    """一个简单的 HTTP 服务器，能够处理 GET 请求"""
+    """一个简单的 HTTP 并发服务器, 能够处理 GET 请求"""
 
     server_name = '0.0.0.0'
     server_port = 9999
@@ -52,19 +57,26 @@ def main():
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # 允许端口重用 
     server_socket.bind((server_name, server_port))                       # 绑定地址和端口
     server_socket.listen(5)                                              # 监听连接请求
-    print(f"HTTP server is running on {server_name}:{server_port}...")
+    print(f"HTTP server is running on {server_name}:{server_port}...\n")
 
+    threads = list()
     try:
         while True:
-            print("Waiting for a connection...")
-            client_socket, client_addr = server_socket.accept()
-            print(f"Connnection from: {client_addr[0]}:{client_addr[1]}")
-            handle_client(client_socket)
+            conn_sock, client_addr = server_socket.accept()
+            t = threading.Thread(target=handle_client, args=(conn_sock,))
+            t.start()
+            threads.append(t)
     except KeyboardInterrupt:
         print("\nServer shutting down...")
     finally:
         server_socket.close()
+        print("Server socket closed.")
+        for t in threads:
+            t.join()
+        print("All threads have finished execution.")
 
 
 if __name__ == '__main__':
+    print(f"{threading.current_thread().name} starting...")
     main()
+    print(f"{threading.current_thread().name} ending...")
