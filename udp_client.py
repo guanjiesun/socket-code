db1b06469d04d836a2becd9ebe4fd7ab34a0c984
guanjie sun <gjsun255@stu.just.edu.cn>
2025-07-15 13:48:55 +0800

    put original code to main function

--- udp_client.py
+++ udp_client.py
@@ -1,20 +1,27 @@
 import socket
 
 
-server_name, server_port = 'localhost', 12000
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-client_socket.settimeout(3)
+def main():
+    """一个简单的 UDP 客户端，连接到服务器，发送消息并接收响应"""
 
-while True:
-    message = input('Input lowercase sentence: ')
-    if message.lower() in ['exit', 'quit']:
-        print('Exiting client...')
-        break
-    try:
-        client_socket.sendto(message.encode(), (server_name, server_port))
-        modified_message, server_address = client_socket.recvfrom(2048)
-        print(f"{server_address[0]}:{server_address[1]} -> {modified_message.decode()}\n")
-    except socket.timeout:
-        print("Request timed out. No response from server. Is the server running?\n")
+    server_name, server_port = '127.0.0.1', 10000
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    client_socket.settimeout(2)
 
-client_socket.close()
+    while True:
+        message = input('Input lowercase sentence: ')
+        if message.lower() in ['exit', 'quit']:
+            print('Exiting client...')
+            break
+        try:
+            client_socket.sendto(message.encode(), (server_name, server_port))
+            modified_message, server_address = client_socket.recvfrom(1024)
+            print(f"{server_address[0]}:{server_address[1]} -> {modified_message.decode()}\n")
+        except socket.timeout:
+            print("Request timed out. No response from server. Is the server running?\n")
+
+    client_socket.close()
+
+
+if __name__ == "__main__":
+    main()
