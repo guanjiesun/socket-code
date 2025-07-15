31cb5d26e905149200fdbca2718cfb0c31bcd492
guanjie sun <gjsun255@stu.just.edu.cn>
2025-07-15 14:16:49 +0800

    update code

--- udp_server.py
+++ udp_server.py
@@ -1,22 +1,27 @@
 import socket
+import threading
+
+
+def handle_client(server_socket):
+    """处理接收到的消息并返回修改后的消息"""
+    message, client_address = server_socket.recvfrom(2048)
+    print(f"[Client ({client_address[0]}:{client_address[1]})]: {message.decode()}")
+    modified_message = message.decode().upper().encode()
+    server_socket.sendto(modified_message, client_address)
 
 
 def main():
+    """一个简单的 UDP 服务器，处理客户端连接(单线程)"""
 
-    server_port, server_name = 12000, '127.0.0.1'
-
+    server_port = 10000
+    server_name = '0.0.0.0'
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     server_socket.bind((server_name, server_port))
     print('The server is ready to receive...')
 
     try:
         while True:
-            message, client_address = server_socket.recvfrom(2048)
-            print(f"Client -> {client_address[0]}:{client_address[1]} -> {message.decode()}")
-
-            modified_message = message.decode().title()
-            server_socket.sendto(modified_message.encode(), client_address)
-            print(f"Server -> {server_name}:{server_port} -> {modified_message}\n")
+            handle_client(server_socket)
     except KeyboardInterrupt:
         print("\nServer shutting down...")
     finally:
@@ -25,4 +30,6 @@ def main():
 
 
 if __name__ == "__main__":
+    print(f"[{threading.current_thread().name}] starting...")
     main()
+    print(f"[{threading.current_thread().name}] ending...")
