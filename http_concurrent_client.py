c18287af659d3db78e7ee137584b24b7829664a3
guanjie sun <gjsun255@stu.just.edu.cn>
2025-07-16 22:24:20 +0800

    update code

--- http_concurrent_client.py
+++ http_concurrent_client.py
@@ -6,14 +6,16 @@ SERVER_ADDR = '127.0.0.1'  # 服务器地址
 SERVER_PORT = 9999         # 服务器端口
 
 
-def send_request(client_socket):
+def send_request(client_socket, path='/'):
+    """发送 HTTP 请求并打印响应"""
+
     client_socket.connect((SERVER_ADDR, SERVER_PORT))
 
     # 构造 HTTP 请求
     request = (
-        "GET / HTTP/1.1\r\n"
+        f"GET {path} HTTP/1.1\r\n"
         f"Host: {SERVER_ADDR}:{SERVER_PORT}\r\n"
-        "Connection: keep-alive\r\n"
+        "Connection: close\r\n"
         "User-Agent: Guanjie's Client\r\n"
         "\r\n"
     )
@@ -28,9 +30,10 @@ def main():
     """一个可以并发请求的 HTTP 客户端，连接到 HTTP 服务器，发送请求并接收响应"""
 
     threads = list()
-    for _ in range(3):
+    paths = ['/index.html', '/json', '/forbidden']
+    for path in paths:
         client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        t = threading.Thread(target=send_request, args=(client_socket,))
+        t = threading.Thread(target=send_request, args=(client_socket, path))
         t.start()
         threads.append(t)
 
