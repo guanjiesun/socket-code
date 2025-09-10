6b6fa07c899cd41a156835bef6c915e3849c75ca
guanjie sun <gjsun255@stu.just.edu.cn>
2025-09-10 23:07:28 +0800

    updated

--- tcp_server.py
+++ tcp_server.py
@@ -1,48 +1,49 @@
 import socket
 import threading
 
+HOST = '0.0.0.0'
+PORT = 18000
 
-def handle_client(conn_socket, client_addr):
-    # 在 TCP 中，每一个 conn_socket 都是由一个四元组唯一标识的: (client_ip, client_port, server_ip, server_port)
-    client_addr = conn_socket.getpeername()
-    server_addr = conn_socket.getsockname()
+def handle_client(conn, client_addr):
+    # 在 TCP 中，每一个 conn 都是由一个四元组唯一标识的: (client_ip, client_port, server_ip, PORT)
+    client_addr = conn.getpeername()
+    server_addr = conn.getsockname()
     print(f"[{threading.current_thread().name}] handling [client {client_addr}]")
     try:
         while True:
-            message = conn_socket.recv(2048)
+            message = conn.recv(2048)
             if not message:
                 print(f"[Client {client_addr}] disconnected.")
                 break
             print(f"[Client {client_addr}]: {message.decode()}")
             modified_message = message.decode().upper()
             print(f"[Server {server_addr}]: {modified_message}\n")
-            conn_socket.send(modified_message.encode())
+            conn.send(modified_message.encode())
     finally:
-        conn_socket.close()
+        conn.close()
 
 
 def main():
     """一个"简单的 TCP 服务器，能够处理多个客户端连接，每个连接在独立的线程中处理"""
 
-    server_name, server_port = '0.0.0.0', 18000
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server_socket.bind((server_name, server_port))
-    server_socket.listen(2)
-    print(f"Server is ready to receive connections on {server_name}:{server_port}\n") 
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind((HOST, PORT))
+    s.listen(2)
+    print(f"Server is ready to receive connections on {HOST}:{PORT}\n") 
 
     threads = list()
     try:
         while True:
-            # 客户端每次连接都会创建一个新的 conn_socket，然后创建一个线程来处理该连接
-            conn_socket, client_addr = server_socket.accept()
-            t = threading.Thread(target=handle_client, args=(conn_socket, client_addr))
+            # 客户端每次连接都会创建一个新的 conn，然后创建一个线程来处理该连接
+            conn, client_addr = s.accept()
+            t = threading.Thread(target=handle_client, args=(conn, client_addr))
             t.start()
             threads.append(t)
     except KeyboardInterrupt:
         print("\nServer shutting down...")  # Ctrl+C 触发的异常
     finally:
-        server_socket.close()               # 关闭服务器套接字, 不再接受新的连接
+        s.close()               # 关闭服务器套接字, 不再接受新的连接
         print("Server socket closed.")
         print("Waiting all threads to finish execution.")
         for t in threads:
