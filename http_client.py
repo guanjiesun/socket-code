3b77ef5a78ad08434930a2bdedae8a49208217de
guanjie sun <gjsun255@stu.just.edu.cn>
2025-09-14 18:14:34 +0800

    make client to be concurrent using threading

--- http_client.py
+++ http_client.py
@@ -1,18 +1,19 @@
 import socket
+import threading
 
 HOST    = '127.0.0.1'
 PORT    = 9999
 CHUNK   = 1024 * 4
 
-def main():
-    """ A HTTP client based on UNIX domain and TCP type socket """
+def worker(path="/"):
+    """ send request and receive response """
     # socket -> connect -> sendall -> recv -> close
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
         client.connect((HOST, PORT))
 
         # build request
         request = (
-            "GET / HTTP/1.1\r\n"
+            f"GET {path} HTTP/1.1\r\n"
             "Host: localhost\r\n"
             "Connection: close\r\n"
             "User-Agent: DIY-Client\r\n"
@@ -21,10 +22,24 @@ def main():
 
         # send request
         client.sendall(request.encode())
-        # receive response
-        response = client.recv(1024)
 
-        print(f"\n{response.decode()}")
+        # receive response
+        response = b""
+        while data := client.recv(CHUNK):
+            response += data
+
+        print(f"{response.decode()}", flush=True)
+
+def main():
+    """ A concurrent HTTP client based on Internet domain and TCP type socket"""
+    paths = ["/", "/json"]
+    threads = [threading.Thread(target=worker, args=(path,)) for path in paths]
+
+    for t in threads:
+        t.start()
+
+    for t in threads:
+        t.join()
 
 if __name__ == "__main__":
     main()
