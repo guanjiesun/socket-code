8c6a42c1b9b87bc63318c0ab03a98944f3df989b
guanjie sun <gjsun255@stu.just.edu.cn>
2025-09-14 16:13:49 +0800

    update the client to be concurrent, using threading module

--- echo-client.py
+++ echo-client.py
@@ -1,21 +1,34 @@
 import socket
+import threading
 
 SERVER_ADDR = '127.0.0.1'
 PORT        = 18000
 CHUNK_SIZE  = 1024 * 4
 
-def main():
-    """ echo 客户端: 基于 AF_INET domain and TCP type 的 socket """
-    # socket -> s.connect -> s.sendall -> s.shutdown -> loop s.recv -> s.close
+def worker(msg):
+    """ create a socket based on AF_INET domain and TCP type """
+    # socket -> s.connect -> s.sendall -> s.shutdown -> s.recv -> s.close
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((SERVER_ADDR, PORT))
-        s.sendall(b"hello")
-        s.shutdown(socket.SHUT_WR)  # if without this line code, the server and client will be deadlocked!
+        s.sendall(msg)
+        # if without shutdown, the server and client will be deadlocked!
+        s.shutdown(socket.SHUT_WR)
 
         response = b""
         while data := s.recv(CHUNK_SIZE):
             response += data
         print(response.decode())
+
+def main():
+    """ A concurrent echo client """
+    messages = [b"hello", b"world", "孙冠杰".encode(), b"Alice"]
+    threads = [threading.Thread(target=worker, args=(msg,)) for msg in messages]
+
+    for t in threads:
+        t.start()
+
+    for t in threads:
+        t.join()
 
 if __name__ == "__main__":
     main()
